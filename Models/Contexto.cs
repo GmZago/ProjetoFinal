7eb3c99b464a43fce722e5b46320249ab19ed26b
GmZago <gm.zago@hotmail.com>
2023-10-26 08:28:13 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoFinal.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }       
+    }
+}
